36b26160982463a104355c7c68767078b202dcbb
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-14 03:14:23 -0300

    Criação da função getAnimalOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const speciesFound = data.species.find((species) => species.name === animal);
+  return speciesFound.residents.every((resident) => resident.age >= age);
 };
-
 module.exports = getAnimalsOlderThan;
