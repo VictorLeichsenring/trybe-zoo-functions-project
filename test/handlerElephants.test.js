7df321f097cb85984d081d22ecf1b6b163d52016
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-14 17:11:32 -0300

    Impĺementados teste para a função handlerElephants superior a 80%

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,13 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it ('Verifica se par o argumento "count" o retorno é 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Verifica se ára o argumento "names" o retorno contem o nome "Jefferson"', () => {
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Verifica se ára o argumento "averageAge" o retorno contem é um número próximo a 10.5', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5, 2);
+  });
+});
