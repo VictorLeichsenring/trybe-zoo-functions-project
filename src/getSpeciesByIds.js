63443775aac7e15ecd503213a691a12548984cc7
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-14 02:52:53 -0300

    Criação da função getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,10 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (!ids.length) {
+    return [];
+  }
+  return data.species.filter((species) => ids.includes(species.id));
 };
 
 module.exports = getSpeciesByIds;
-// iniciando o projeto
