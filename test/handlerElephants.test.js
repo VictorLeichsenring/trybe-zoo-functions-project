6062a78ab16241f758f7e7f5ee6dd9b32aa81482
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-14 19:31:23 -0300

    Correções lint

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,22 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
-  it ('Verifica se par o argumento "count" o retorno é 4', () => {
+  it('Verifica se para o argumento "count" o retorno é 4', () => {
     expect(handlerElephants('count')).toBe(4);
   });
-  it('Verifica se para o argumento "names" o retorno contem o nome "Jefferson"', () => {
+  it('Verifica se para o argumento "names" o retorno contém o nome "Jefferson"', () => {
     expect(handlerElephants('names')).toContain('Jefferson');
   });
-  it('Verifica se para o argumento "averageAge" o retorno contem é um número próximo a 10.5', () => {
+  it('Verifica se para o argumento "averageAge" o retorno é um número próximo a 10.5', () => {
     expect(handlerElephants('averageAge')).toBeCloseTo(10.5, 2);
   });
-  it('Verifica se para o argumento "location" o retorno contem é "NW"', () => {
+  it('Verifica se para o argumento "location" o retorno é "NW"', () => {
     expect(handlerElephants('location')).toBe('NW');
   });
-  it('Verifica se para o argumento "availability" o retorno é array de dias da semana que não contém Monday', () => {
+  it('Verifica se para o argumento "availability" o retorno é um array de dias da semana que não contém "Monday"', () => {
     expect(handlerElephants('availability')).not.toContain('Monday');
   });
-  it('Verifica se não for passado argumento o retorno é undefined', () => {
+  it('Verifica se não for passado argumento, o retorno é undefined', () => {
     expect(handlerElephants()).toBeUndefined();
   });
 });
