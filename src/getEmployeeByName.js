8d365513e2b6a10cc064f4e70e75b1ea94a70284
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-14 14:27:00 -0300

    Criação da função getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const employeeFound = data.employees.find((employee) => {
+    const isFirstName = employeeName === employee.firstName;
+    const isLastName = employeeName === employee.lastName;
+
+    return isFirstName || isLastName;
+  });
+  return employeeFound;
 };
 
 module.exports = getEmployeeByName;
