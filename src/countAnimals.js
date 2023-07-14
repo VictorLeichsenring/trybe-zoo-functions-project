7916cbb8fb40296cc94242ddf4ab430228e3be26
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-14 16:26:33 -0300

    Criação da função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  let result = {};
+  if (!animal) {
+    result = data.species.reduce((acc, index) => {
+      acc[index.name] = index.residents.length;
+      return acc;
+    }, {});
+  } else {
+    const { species } = animal;
+    const { sex } = animal;
+    const targetSpecies = data.species.find((speciesItem) => speciesItem.name === species);
+    if (sex) {
+      result = targetSpecies.residents.filter((resident) => resident.sex === sex).length;
+    } else {
+      result = targetSpecies.residents.length;
+    }
+  }
+  return result;
 };
 
 module.exports = countAnimals;
