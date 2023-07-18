abd479a3c0537710226e25f61d2635cd526b3eee
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-18 04:14:25 -0300

    Testes da função getOpeningHours em 100%

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -57,4 +57,14 @@ describe('Testes da função getOpeningHours', () => {
       getOpeningHours('Sunday', '09:c0-AM');
     }).toThrow('The minutes should represent a number');
   });
+  it('Para os argumentos Monday e 13:00-AM deve lançar uma exceção com a mensagem "The minutes should represent a number"', () => {
+    expect(() => {
+      getOpeningHours('Monday', '13:00-AM');
+    }).toThrow('The hour must be between 0 and 12');
+  });
+  it('Para os argumentos Tuesday e 09:60-AM deve lançar uma exceção com a mensagem: "The minutes must be between 0 and 59"', () => {
+    expect(() => {
+      getOpeningHours('Tuesday', '09:60-AM');
+    }).toThrow('The minutes must be between 0 and 59');
+  });
 });
