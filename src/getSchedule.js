ec4f5c0edb42daf474639545a97455aaf6297707
Victor Leichsenring <victor.leichsenring@gmail.com>
2023-07-18 21:27:46 -0300

    Simplificada a função

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,85 +1,65 @@
 const data = require('../data/zoo_data');
 
 const weekDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+function identifyAnimal(string) {
+  const animalFound = data.species.find((animal) => animal.name === string);
+  return animalFound;
+}
 
-const buildDailySchedule = (day) => {
+function createDay(day) {
   const animals = data.species
     .filter((specie) => specie.availability.includes(day))
     .map((specie) => specie.name);
-
-  const officeHour = [
-    `Open from ${data.hours[day].open}am`,
-    `until ${data.hours[day].close}pm`,
-  ].join(' ');
-
+  const officeHour = `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
   return {
     officeHour,
     exhibition: animals,
   };
-};
+}
 
-const buildSchedule = () => {
+function withoutParameters() {
   const schedule = {};
   weekDays.forEach((day) => {
-    schedule[day] = buildDailySchedule(day);
+    schedule[day] = createDay(day);
   });
-
   schedule.Monday = {
     officeHour: 'CLOSED',
     exhibition: 'The zoo will be closed!',
   };
-
   return schedule;
-};
+}
 
-const getAnimalsForDay = (day) => {
-  if (!day || day === 'Monday') return {};
-
-  const animals = data.species
-    .filter((specie) => specie.availability.includes(day))
-    .map((specie) => specie.name);
-
-  const officeHour = [
-    `Open from ${data.hours[day].open}am`,
-    `until ${data.hours[day].close}pm`,
-  ].join(' ');
-
+function isMonday() {
   return {
-    [day]: {
-      officeHour,
-      exhibition: animals,
-    },
-  };
-};
-
-const getAvailabilityForSpecies = (species) => {
-  if (!species) return {};
-
-  const speciesData = data.species.find((element) => element.name === species);
-  return speciesData.availability;
-};
-
-const strategies = {
-  default: buildSchedule,
-  Monday: () => ({
     Monday: {
       officeHour: 'CLOSED',
       exhibition: 'The zoo will be closed!',
     },
-  }),
-  weekDays: getAnimalsForDay,
-  species: getAvailabilityForSpecies,
-};
+  };
+}
 
 const getSchedule = (scheduleTarget) => {
-  const species = data.species.map((specie) => specie.name);
-  let strategy = 'default';
+  if (scheduleTarget === 'Monday') {
+    return isMonday();
+  }
 
-  if (scheduleTarget === 'Monday') strategy = 'Monday';
-  else if (weekDays.includes(scheduleTarget)) strategy = 'weekDays';
-  else if (species.includes(scheduleTarget)) strategy = 'species';
+  if (!scheduleTarget) {
+    return withoutParameters();
+  }
 
-  return strategies[strategy](scheduleTarget);
+  const animal = identifyAnimal(scheduleTarget);
+  if (animal) {
+    return animal.availability;
+  }
+
+  if (weekDays.includes(scheduleTarget)) {
+    const schedule = {
+      [scheduleTarget]: createDay(scheduleTarget),
+    };
+    return schedule;
+  }
+
+  return withoutParameters();
 };
 
 module.exports = getSchedule;
