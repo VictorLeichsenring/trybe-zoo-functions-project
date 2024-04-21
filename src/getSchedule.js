f76dbb7baf15c0fa7eddec32bc78243144fb9412
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-21 16:02:09 -0300

    commit para publicação

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
+//Criada uma constante para armazenar os dias da semana
 const weekDays = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+// Criada função para identificar qual a especie
 function identifyAnimal(string) {
   const animalFound = data.species.find((animal) => animal.name === string);
   return animalFound;
 }
 
+// Criada função que cria um objeto de acordo com o dia recebido
 function createDay(day) {
   const animals = data.species
     .filter((specie) => specie.availability.includes(day))
@@ -17,6 +21,7 @@ function createDay(day) {
   };
 }
 
+// criada função que itera sobre os dias da semana e retorna um objeto com varios dias
 function withoutParameters() {
   const schedule = {};
   weekDays.forEach((day) => {
@@ -29,6 +34,7 @@ function withoutParameters() {
   return schedule;
 }
 
+// Criada função que retorna o Domingo
 function isMonday() {
   return {
     Monday: {
@@ -38,27 +44,30 @@ function isMonday() {
   };
 }
 
+// função principal que irá chamar as demais funções
 const getSchedule = (scheduleTarget) => {
+  //Se for domingo
   if (scheduleTarget === 'Monday') {
     return isMonday();
   }
 
+  // se o parametro for vazio
   if (!scheduleTarget) {
     return withoutParameters();
   }
-
+  // armazena o animal
   const animal = identifyAnimal(scheduleTarget);
   if (animal) {
     return animal.availability;
   }
-
+  // se for dia da semana
   if (weekDays.includes(scheduleTarget)) {
     const schedule = {
       [scheduleTarget]: createDay(scheduleTarget),
     };
     return schedule;
   }
-
+  // demais situações
   return withoutParameters();
 };
 
